adc6500a8fe1d179fb48f4c271141a20312d48ae
Mahesh Voorugonda <mvooru@codeaurora.org>
2020-02-25 10:44:15 +0530

    mm-video-v4l2: venc: Enable USE_NATIVE_HANDLE_SOURCE
    Enable USE_NATIVE_HANDLE_SOURCE Macro to use meta
    buffer type as kMetadataBufferTypeNativeHandleSource.
    
    Change-Id: I81330c2b5e476ef3e091dd72c021136d6773c390

--- mm-video-v4l2/vidc/venc/Android.mk
+++ mm-video-v4l2/vidc/venc/Android.mk
@@ -20,6 +20,7 @@ libmm-venc-def += -USINGLE_ENCODER_INSTANCE
 libmm-venc-def += -Werror
 libmm-venc-def += -D_ANDROID_ICS_
 libmm-venc-def += -D_MSM8974_
+libmm-venc-def += -DUSE_NATIVE_HANDLE_SOURCE
 
 TARGETS_THAT_USE_FLAG_MSM8226 := msm8226 msm8916 msm8909
 TARGETS_THAT_NEED_SW_VENC_MPEG4 := msm8909 msm8937
